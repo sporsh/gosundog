716eeff50bcd1790e71899e05fe36ae9131b335b
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-04-28 22:38:22 +0200

    define intersectable geometry

---
+++ geometry/geometry.go
@@ -0,0 +1,12 @@
+package geometry
+
+import "github.com/sporsh/gosundog/v3"
+
+type Intersectable interface {
+	Intersect(r Ray, epsilon float64) (i Intersection, ok bool)
+}
+
+type Intersection struct {
+	T             float64
+	Point, Normal v3.V
+}
