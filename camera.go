4495c203ac70f8b882cc577bc4216eee5533446c
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-04-29 14:01:52 +0200

    add methods to calculate a ray into the scene from screen coordinates
    as seen from the camera in world space

--- camera.go
+++ camera.go
@@ -14,14 +14,36 @@ type Camera struct {
 	Aperture float64
 }
 
-func (c Camera) LookAt(p v3.V) {}
+// RayThrough computes a ray in the world vector space
+// that corresponds to the x, y screen space as seen from the camera
+func (c Camera) RayThrough(x, y int) geometry.Ray {
+	origin := c.RandomOriginWithinAperture()
+	target := c.Basis.ToLocal(
+		v3.Normalize(v3.V{float64(x), float64(y), 1}),
+	)
+	direction := v3.Sub(target, origin)
+	return geometry.Ray{
+		Direction: direction,
+		Origin:    origin,
+		TMin:      0,
+		TMax:      math.Inf(1),
+	}
+}
 
+// LookAt reorients the camera's basis so that it is looking straight at a target
+func (c Camera) LookAt(target v3.V) {
+
+}
+
+// RandomOriginWithinAperture returns, as a three dimensional vector, a point
+// in world space that lies within the camera's aperture
 func (c Camera) RandomOriginWithinAperture() v3.V {
 	u1, u2 := rand.Float64(), rand.Float64()
 	r := u2 * c.Aperture
+	theta := u1 * 2 * math.Pi
 	localPoint := v3.V{
-		math.Cos(u1*2*math.Pi) * r,
-		math.Sin(u1*2*math.Pi) * r,
+		math.Cos(theta) * r,
+		math.Sin(theta) * r,
 		0,
 	}
 	return v3.Add(
