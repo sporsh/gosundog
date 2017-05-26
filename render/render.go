765469ef7fe83ff13841209b3fad5a842f25a4e0
Geir Sporsheim <geir.sporsheim@gmail.com>
2017-05-26 21:07:04 +0200

    add script for rendering hard coded scene

---
+++ render/render.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"image/png"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	sundog "github.com/sporsh/gosundog"
+	"github.com/sporsh/gosundog/geometry"
+	"github.com/sporsh/gosundog/v3"
+)
+
+func main() {
+	rand.Seed(0)
+
+	lightMaterial := sundog.NewLambertianMaterial(v3.V{0.5, 0.5, 2}, v3.V{0.8, 0.8, 0.8})
+	whiteMaterial := sundog.NewLambertianMaterial(v3.ZERO, v3.V{0.8, 0.8, 0.8})
+	mirrorMaterial := sundog.NewSpecularMaterial(v3.ZERO, v3.V{0.8, 0.8, 0.8})
+	redMaterial := sundog.NewLambertianMaterial(v3.ZERO, v3.V{0.75, 0.25, 0.25})
+	greenMaterial := sundog.NewLambertianMaterial(v3.ZERO, v3.V{0.25, 0.75, 0.25})
+
+	g := &geometry.Group{
+		// sundog.Renderable{
+		// 	Intersectable: geometry.NewSphere(v3.V{-0.55, -0.5, 0}, 0.25),
+		// 	Material:      redMaterial,
+		// },
+		sundog.Renderable{
+			Intersectable: geometry.NewSphere(v3.V{-0.5, -0.5, 0}, .3),
+			Material:      whiteMaterial,
+		},
+		sundog.Renderable{
+			Intersectable: geometry.NewSphere(v3.V{0, 0.15, 0.3}, .3),
+			Material:      mirrorMaterial,
+		},
+		sundog.Renderable{
+			Intersectable: geometry.NewSphere(v3.V{0.5, -0.5, 0}, .3),
+			Material:      lightMaterial,
+		},
+		// sundog.Renderable{
+		// 	Intersectable: geometry.NewSphere(v3.V{0.55, -0.5, 0}, 0.25),
+		// 	Material:      greenMaterial,
+		// },
+
+		// Cornell box
+		sundog.Renderable{
+			Intersectable: geometry.NewPlane(v3.V{1, 0, 0}, -1),
+			Material:      redMaterial,
+		},
+		sundog.Renderable{
+			Intersectable: geometry.NewPlane(v3.V{-1, 0, 0}, -1),
+			Material:      greenMaterial,
+		},
+		sundog.Renderable{
+			Intersectable: geometry.NewPlane(v3.V{0, 0, -1}, -1),
+			Material:      whiteMaterial,
+		},
+		sundog.Renderable{
+			Intersectable: geometry.NewPlane(v3.V{0, 1, 0}, -1),
+			Material:      whiteMaterial,
+		},
+		sundog.Renderable{
+			Intersectable: geometry.NewPlane(v3.V{0, -1, 0}, -1),
+			Material:      whiteMaterial,
+		},
+
+		// Light
+		// sundog.Renderable{
+		// 	Intersectable: geometry.NewSphere(v3.V{0, 1, 0}, 0.25),
+		// 	Material:      lightMaterial,
+		// },
+	}
+
+	c := sundog.Camera{
+		Origin: v3.V{0, 0, -1.5},
+		Basis: geometry.Basis{
+			Tangent:   v3.X,
+			Bitangent: v3.Y,
+			Normal:    v3.Z,
+		},
+		Aperture: 0,
+		// Aperture:    0.05,
+		FieldOfView: 1,
+		FocalLength: 1.5,
+	}
+
+	f, err := os.Create("image.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img := sundog.NewPathTraceImage(g, c, 400, 400)
+
+	t0 := time.Now()
+	png.Encode(f, img)
+	log.Println(time.Since(t0))
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
